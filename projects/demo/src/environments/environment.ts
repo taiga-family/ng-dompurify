a8bc5e2dbda41397744f73363f8ec3257f572700
mdlufy <german.panov.2018@mail.ru>
2025-06-21 01:15:45 +0300

    chore(demo): configure ssr

---
+++ projects/demo/src/environments/environment.ts
@@ -0,0 +1,6 @@
+import {environment as envCommon} from './environment.prod';
+
+export const environment = {
+    ...envCommon,
+    production: false,
+};
---
+++ projects/demo/src/main.server.ts
@@ -0,0 +1,22 @@
+import type {ApplicationRef} from '@angular/core';
+import {ErrorHandler, importProvidersFrom, mergeApplicationConfig} from '@angular/core';
+import {bootstrapApplication} from '@angular/platform-browser';
+import {provideServerRendering, ServerModule} from '@angular/platform-server';
+
+import {AppComponent} from './app/app.component';
+import {appConfig} from './app/app.config';
+import {ServerErrorHandler} from './app/server-error-handler';
+
+const serverConfig = mergeApplicationConfig(appConfig, {
+    providers: [
+        importProvidersFrom(ServerModule),
+        provideServerRendering(),
+        {
+            provide: ErrorHandler,
+            useClass: ServerErrorHandler,
+        },
+    ],
+});
+
+export default async (): Promise<ApplicationRef> =>
+    bootstrapApplication(AppComponent, serverConfig);
