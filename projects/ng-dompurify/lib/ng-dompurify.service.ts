e2309466f3bfee34516672909cf14735d694b00c
mdlufy <german.panov.2018@mail.ru>
2025-06-23 15:14:03 +0300

    feat: update dompufiry to 3.2.6

--- projects/ng-dompurify/lib/ng-dompurify.service.ts
+++ projects/ng-dompurify/lib/ng-dompurify.service.ts
@@ -1,13 +1,22 @@
 import {DOCUMENT} from '@angular/common';
 import type {Sanitizer} from '@angular/core';
 import {inject, Injectable, SecurityContext} from '@angular/core';
-import type {DOMPurifyI} from 'dompurify';
+import type {
+    DocumentFragmentHook,
+    DOMPurify,
+    ElementHook,
+    HookName,
+    NodeHook,
+    UponSanitizeAttributeHook,
+    UponSanitizeElementHook,
+} from 'dompurify';
 import dompurify from 'dompurify';
 
 import {DOMPURIFY_CONFIG} from './tokens/dompurify-config';
 import {DOMPURIFY_HOOKS} from './tokens/dompurify-hooks';
 import {SANITIZE_STYLE} from './tokens/sanitize-style';
 import type {NgDompurifyConfig} from './types/ng-dompurify-config';
+import type {NgDompurifyHook} from './types/ng-dompurify-hook';
 
 const createDOMPurify = dompurify;
 
@@ -27,13 +36,15 @@ const createDOMPurify = dompurify;
 export class NgDompurifySanitizer implements Sanitizer {
     private readonly config = inject(DOMPURIFY_CONFIG);
     private readonly sanitizeStyle = inject(SANITIZE_STYLE);
-    private readonly domPurify: DOMPurifyI;
+    private readonly domPurify: DOMPurify;
 
     constructor() {
-        this.domPurify = createDOMPurify(inject(DOCUMENT).defaultView as Window);
+        this.domPurify = createDOMPurify(
+            inject(DOCUMENT).defaultView as dompurify.WindowLike,
+        );
 
         inject(DOMPURIFY_HOOKS).forEach(({name, hook}) => {
-            this.domPurify.addHook(name, hook);
+            this.addHook(name, hook);
         });
     }
 
@@ -49,5 +60,33 @@ export class NgDompurifySanitizer implements Sanitizer {
         return context === SecurityContext.STYLE
             ? this.sanitizeStyle(String(value))
             : this.domPurify.sanitize(String(value || ''), config);
+    }
+
+    private addHook(name: HookName, hook: NgDompurifyHook['hook']): void {
+        switch (name) {
+            case 'afterSanitizeAttributes':
+            case 'beforeSanitizeAttributes':
+                this.domPurify.addHook(name, hook as ElementHook);
+                break;
+
+            case 'afterSanitizeElements':
+            case 'beforeSanitizeElements':
+            case 'uponSanitizeShadowNode':
+                this.domPurify.addHook(name, hook as NodeHook);
+                break;
+
+            case 'afterSanitizeShadowDOM':
+            case 'beforeSanitizeShadowDOM':
+                this.domPurify.addHook(name, hook as DocumentFragmentHook);
+                break;
+
+            case 'uponSanitizeAttribute':
+                this.domPurify.addHook(name, hook as UponSanitizeAttributeHook);
+                break;
+
+            case 'uponSanitizeElement':
+                this.domPurify.addHook(name, hook as UponSanitizeElementHook);
+                break;
+        }
     }
 }
--- projects/ng-dompurify/lib/types/ng-dompurify-config.ts
+++ projects/ng-dompurify/lib/types/ng-dompurify-config.ts
@@ -3,7 +3,19 @@ import type {Config} from 'dompurify';
 /**
  * Config that allows to return only string result
  */
-export type NgDompurifyConfig = Config & {
-    RETURN_DOM_FRAGMENT?: false;
-    RETURN_DOM?: false;
+export type NgDompurifyConfig = Omit<
+    Config,
+    'IN_PLACE' | 'RETURN_DOM_FRAGMENT' | 'RETURN_DOM' | 'RETURN_TRUSTED_TYPE'
+> & {
+    /**
+     * Disallow DOM return options to ensure string output
+     */
+    RETURN_DOM?: never;
+    RETURN_DOM_FRAGMENT?: never;
+    RETURN_TRUSTED_TYPE?: never;
+
+    /**
+     * Disallow in-place sanitization as we always use string input
+     */
+    IN_PLACE?: never;
 };
