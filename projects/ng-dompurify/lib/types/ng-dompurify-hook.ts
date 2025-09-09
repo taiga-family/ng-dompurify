5bdc7f6c4db85784596daf3a1aa24d49564b10ee
mdlufy <german.panov.2018@mail.ru>
2025-09-09 16:03:15 +0300

    chore: update 19 angular

--- projects/ng-dompurify/lib/types/ng-dompurify-hook.ts
+++ projects/ng-dompurify/lib/types/ng-dompurify-hook.ts
@@ -19,27 +19,3 @@ export interface NgDompurifyHook {
         | UponSanitizeAttributeHook
         | UponSanitizeElementHook;
 }
-
-// export type BasicHookName =
-//     | 'afterSanitizeElements'
-//     | 'beforeSanitizeElements'
-//     | 'uponSanitizeShadowNode';
-// export type ElementHookName = 'afterSanitizeAttributes' | 'beforeSanitizeAttributes';
-// export type DocumentFragmentHookName =
-//     | 'afterSanitizeShadowDOM'
-//     | 'beforeSanitizeShadowDOM';
-
-// /**
-//  * A DOMPurify supported hook see {@link addHook}
-//  */
-// export type NgDompurifyHook<T extends HookName = HookName> = T extends BasicHookName
-//     ? {readonly name: T; readonly hook: NodeHook}
-//     : T extends ElementHookName
-//       ? {readonly name: T; readonly hook: ElementHook}
-//       : T extends DocumentFragmentHookName
-//         ? {readonly name: T; readonly hook: DocumentFragmentHook}
-//         : T extends 'uponSanitizeElement'
-//           ? {readonly name: T; readonly hook: UponSanitizeElementHook}
-//           : T extends 'uponSanitizeAttribute'
-//             ? {readonly name: T; readonly hook: UponSanitizeAttributeHook}
-//             : never;
