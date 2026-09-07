f76c15132dee3e7e84282dff3e5cc0fb0b244718
Максим Иванов <omaxphp@yandex.ru>
2026-09-07 13:16:12 +0300

    chore: update ESLint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,6 +2,7 @@ import taiga from '@taiga-ui/eslint-plugin-experience-next';
 
 export default [
     ...taiga.configs.recommended,
+    ...taiga.configs.jest,
     {
         files: ['CHANGELOG.md'],
         rules: {'markdown/heading-increment': 'off'},
